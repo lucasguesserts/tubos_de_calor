f83fcd62683aca5d3634a88dc294306d63c14f59
Lucas Guesser Targino da Silva <guesser@sinmec.ufsc.br>
2017-09-19 07:49:34 -0300

    fluid quantity revised.

--- python/fluid_quantity.py
+++ python/fluid_quantity.py
@@ -13,9 +13,21 @@ volume_environment = mass_total / rho_environment
 volume_environment_in_ml = volume_environment * 1E+6
 
 def print_fluid_quantity(message, quantity_in_ml):
-    print("{:45} = {:4.3} [ml]".format(message, quantity_in_ml))
+    print("{:45} = {:4.3g} [ml]".format(message, quantity_in_ml))
 
-print_fluid_quantity("Volume necessario", volume_environment_in_ml)
-print_fluid_quantity("110% do Volume necessario", 1.1*volume_environment_in_ml)
+print_fluid_quantity("epsilon",epsilon)
+print_fluid_quantity("area_liquid * pipe_length", 1E+6*(area_liquid*pipe_length))
+print("\n")
+
+print_fluid_quantity("volume ocupado pelo liquido", 1E+6*volume_liquid)
+print_fluid_quantity("volume ocupado pelo vapor", 1E+6*volume_steam)
+print("\n")
+
+print("Proposta do Nelson:")
 print_fluid_quantity("Volume necessario no meio poroso:", 1E+6*volume_liquid)
 print_fluid_quantity("110% do volume necessario no meio poroso:", 1E+6*1.1*volume_liquid)
+print("\n")
+
+print("Nossa proposta:")
+print_fluid_quantity("Volume necessario", volume_environment_in_ml)
+print_fluid_quantity("110% do Volume necessario", 1.1*volume_environment_in_ml)
