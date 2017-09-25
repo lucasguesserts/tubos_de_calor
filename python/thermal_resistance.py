7d6a26eb29295e1d844e4daa60dea6a6ce4833a7
guesser <lucasguesserts@gmail.com>
2017-09-24 21:42:22 -0300

    condenser external resistance added.

--- python/thermal_resistance.py
+++ python/thermal_resistance.py
@@ -7,11 +7,58 @@ from CoolProp.CoolProp import PropsSI
 # how it is build.
 R_ext_e = 0
 
-# A precise model is needed for
-# external condenser resistance;
-# It will depend on our system,
-# how it is build.
-R_ext_c = 0
+# condenser resistance
+def correlation_C(Re_D):
+    if  0.4 <= Re_D < 4.0:
+        return 0.989
+    elif 4.0 <= Re_D < 40:
+        return 0.911
+    elif 40 <= Re_D < 4000:
+        return 0.683
+    elif 4000 <= Re_D < 40000:
+        return 0.193
+    elif 40000 <= Re_D < 400000:
+        return 0.027
+    else:
+        raise ValueError("invalid Re_D value inserted in function \'C\'")
+
+def correlation_m(Re_D):
+    if  0.4 <= Re_D < 4.0:
+        return 0.330
+    elif 4.0 <= Re_D < 40:
+        return 0.385
+    elif 40 <= Re_D < 4000:
+        return 0.466
+    elif 4000 <= Re_D < 40000:
+        return 0.618
+    elif 40000 <= Re_D < 400000:
+        return 0.805
+    else:
+        raise ValueError("invalid Re_D value inserted in function \'m\'")
+
+def h(V, D, temperature):
+    fluid = "Air"
+    pressure = 101325
+    k = PropsSI("L", "T", temperature, "P", pressure, fluid)
+    cp = PropsSI("C", "T", temperature, "P", pressure, fluid)
+    rho = PropsSI("D", "T", temperature, "P", pressure, fluid)
+    mu = PropsSI("V", "T", temperature, "P", pressure, fluid)
+
+    Re_D = rho * V * D / mu
+    Pr = cp * mu / k
+    C = correlation_C(Re_D)
+    m = correlation_m(Re_D)
+
+    Nu = C * Re_D**m * Pr**(1/3)
+    h = Nu * k / D
+    return h
+
+fluid_velocity = 7.0
+condenser_area = numpy.pi * pipe_external_diameter**2 / 4
+environment_temperature = 25 + 273.15
+film_temperature = (environment_temperature + temperature_operation) / 2
+h_condenser = h(fluid_velocity, pipe_external_diameter, film_temperature)
+R_ext_c = 1 / (h_condenser * condenser_area)
 
 # Needed properties
 R = 8.3144621 # [J/(K mol)]
@@ -43,6 +90,7 @@ Rp = Rpe/2 + Rpa + Rpc/2
 Rw = Rpe + Rwe/2 + Rwa + Rwc/2 + Rpc
 Rv = Rpe + Rwe + Rie + Rva + Ric + Rwc + Rpc
 Rt = R_ext_e + (1/Rp + 1/Rw + 1/Rv)**(-1) + R_ext_c
+
 
 if __name__ == "__main__":
 
